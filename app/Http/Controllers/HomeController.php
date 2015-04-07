e160de76cdc774a64637e38cf2c528ae8061cc0b
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-07 08:53:49 -0300

    remove unused routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,5 @@
 <?php namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\File;
-
 class HomeController extends Controller {
 
 	/*
--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,5 +1,7 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Log;
+
 class WelcomeController extends Controller {
 
 	/*
